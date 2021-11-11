fcb74fbd3ba757a667cc99356cab0974b769a685
Zevety <90498783+Zevety@users.noreply.github.com>
2021-11-11 20:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
+import random
+import time
+from multiprocessing import Process, Pool, Event
+import keyboard
 
 
-def element(index, A, B):
+# определение элемента
+def element(index, A, B, size):
     i, j = index
     res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
+    for k in range(size):
         res += A[i][k] * B[k][j]
     return res
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+# установка размера матрицы
+def set_size(size):
+    return [[random.randint(-100, 100) for j in range(size)] for i in range(size)]
 
-print(element((1, 0), matrix1, matrix2))
+
+# запись матрицы c (почти) уникальным ID
+def write_out(mat, name):
+    for idents, item in enumerate(mat):
+        with open(f'{name[0]}_{random.randint(1, 1000)}.txt', 'w') as file:
+            file.write(f"Matrix {idents}:\n")
+            for i in item:
+                file.write(' '.join([str(j) for j in i]) + '\n')
+
+
+# получение результата
+def get_result(arr, size, pool):
+    for i in range(size):
+        for j in range(size):
+            result = pool.apply_async(element, ((i, j), arr[0], arr[1], size))
+            res_matrix = result.get()
+            arr[2][i].append(res_matrix)
+    return arr[2]
+
+
+# основное вычисление c автоматическим выборо кол-ва процессов
+def general_operations(size, ev_stop):
+    while ev_stop.is_set():
+        with Pool(processes=size * 2) as pool:
+            array1, array2 = set_size(size), set_size(size)
+            write_out([array1, array2], ['matrix_1', 'matrix_2'])
+            array3 = [[] for i in range(size)]
+            array3 = get_result([array1, array2, array3], size, pool)
+            write_out([array3], ['matrix_3'])
+            time.sleep(5)
+
+
+# основная функция с остановкой процесса и заданием размера матрицы
+def main():
+    ev = Event()
+    ev.set()
+    size = int(input("Задайте размер матрицам: "))
+    main_event = Process(target=general_operations, args=(size, ev))
+    main_event.start()
+    while keyboard.read_key() == "p":
+        print("Exiting from program")
+        break
+    ev.clear()
+    main_event.join()
+
+
+if __name__ == '__main__':
+    main()
